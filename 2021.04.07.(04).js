fea5dc04123f586e6489487ba677d14ba5950087
ElaineLee21 <playder1427@gmail.com>
2021-06-19 09:33:33 +0900

    프로그래머스 - 서울에서 김서방 찾기

---
+++ 2021.04.07.(04).js
@@ -0,0 +1,11 @@
+// 프로그래머스 - 서울에서 김서방 찾기
+
+// 나의 풀이
+function solution(seoul) {
+   // indexof("Kim")
+   var answer = '';
+   
+   answer = `김서방은 ${seoul.indexOf("Kim")}에 있다`
+   
+   return answer;
+}
